40464ac82453690df281ce664a1de3cfb751ef16
Andre1876 <lawrenceaj86@gmail.com>
2021-07-02 12:08:27 -0400

    add index.js file, move js codes from html file to js file

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 let myText = 'I am a string';
 let newString = myText.replace('string', 'sausage');
 console.log(newString);
+
+   // JavaScript Notes!
+
+        // This is my first JavaScript code!
+
+        let a = 10
+        console.log(a)
+
+        console.log (9*a) 
+
+        let b = 7 * a 
+        console.log(b)
+
+        var max = 57
+        var actual = max - 13
+        var percentage = actual / max
+        console.log(percentage)
+        
+        console.log("Andre!")
+        console.log("Hello, World!")
+        console.log(23 + 97)
+        console.log(4+6+9 / 77 )
